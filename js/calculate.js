440136d19492ebf94214830fdb4aacde4c0074fa
Mikael <m.mikael.eriksson@gmail.com>
2017-10-05 21:29:11 +0200

    Added flickclock that counts down to endTime from currentTime.

--- js/calculate.js
+++ js/calculate.js
@@ -22,4 +22,30 @@ function updateEndTime() {
     minutes = (minutes<10?'0':'') + minutes;
 
     $("#endTime").val(hours + ":" + minutes);
+
+    startCountDown();
+}
+
+
+function startCountDown() {
+    var currentDate = new Date(Date.now());
+    var endTime = $("#endTime").val();
+    endTime = endTime.split(":");
+
+    var endDate = new Date();
+    endDate.setHours(endTime[0]);
+    endDate.setMinutes(endTime[1]);
+
+    currentMinutes = ((currentDate.getHours() * 60) +
+		      currentDate.getMinutes());
+
+    endMinutes = ((endDate.getHours() * 60) +
+		  endDate.getMinutes());
+
+    var countdownMinutes = (endMinutes - currentMinutes);
+    countdownMinutes = countdownMinutes < 0 ? 0 : countdownMinutes;
+
+    var clock = $('.clock').FlipClock((countdownMinutes * 60), {
+	countdown: true
+    });
 }
